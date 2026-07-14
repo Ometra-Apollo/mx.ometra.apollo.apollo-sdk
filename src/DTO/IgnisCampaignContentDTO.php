f33b1fdf5821c8ed4cc21bf8d0cf5b57bd2dfa26
Guillermo Corona Muñoz <memoola49@gmail.com>
2026-07-14 10:48:13 -0600

    feat: :sparkles: add media_type and ext properties to IgnisCampaignContentDTO

--- src/DTO/IgnisCampaignContentDTO.php
+++ src/DTO/IgnisCampaignContentDTO.php
@@ -12,6 +12,8 @@ final class IgnisCampaignContentDTO
     public function __construct(
         public readonly string $id_media,
         public readonly string $type,
+        public readonly string $media_type,
+        public readonly string $ext,
         public readonly string $name,
         public readonly array $content_schedules,
     ) {}
@@ -24,6 +26,8 @@ final class IgnisCampaignContentDTO
         return new self(
             id_media: (string) $data['id_media'],
             type: (string) $data['type'],
+            media_type: (string) ($data['media_type'] ?? $data['type'] ?? ''),
+            ext: (string) ($data['ext'] ?? ''),
             name: (string) $data['name'],
             content_schedules: array_map(
                 static fn (array $schedule): IgnisCampaignContentScheduleDTO => IgnisCampaignContentScheduleDTO::fromArray($schedule),
@@ -36,13 +40,14 @@ final class IgnisCampaignContentDTO
     }
 
     /**
-     * @return array{id_media:string,type:string,name:string,content_schedules:array<int,array{dt_start:string,dt_end:string,dow:array<int,string>}>}
+     * @return array{id_media:string,type:string,media_type:string,ext:string,name:string,content_schedules:array<int,array{dt_start:string,dt_end:string,dow:array<int,string>}>}
      */
     public function toArray(): array
     {
         return [
             'id_media' => $this->id_media,
-            'type' => $this->type,
+            'media_type' => $this->media_type,
+            'ext' => $this->ext,
             'name' => $this->name,
             'content_schedules' => array_map(
                 static fn (IgnisCampaignContentScheduleDTO $schedule): array => $schedule->toArray(),
