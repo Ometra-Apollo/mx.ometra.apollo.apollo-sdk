aed2bca3763c7f8e6422225b99d4e0ac63f505f0
Karla Oaxaca <54610989+karlyoz@users.noreply.github.com>
2026-08-03 16:21:01 -0600

    feat: add PlaylistLightPathResource for managing lightpath operations in playlists

--- src/Modules/Flare/Resources/PlaylistItemsResource.php
+++ src/Modules/Flare/Resources/PlaylistItemsResource.php
@@ -6,16 +6,42 @@ namespace Ometra\Apollo\Sdk\Modules\Flare\Resources;
 
 use Ometra\Apollo\Sdk\Core\Http\ApolloHttpClient;
 
+/**
+ * Resource for managing items within a playlist
+ *
+ * Provides methods to retrieve and manage media items
+ * that belong to a specific playlist.
+ */
 final class PlaylistItemsResource
 {
+    /**
+     * Initializes the playlist items resource
+     *
+     * @param ApolloHttpClient $client Apollo HTTP client
+     * @param string $playlistId The playlist ID
+     */
     public function __construct(
         private readonly ApolloHttpClient $client,
         private readonly string $playlistId,
     ) {}
 
-    /** @return array<array-key, mixed> */
+    /**
+     * Retrieves all items in the playlist
+     *
+     * @return array<array-key, mixed> List of media items in the playlist
+     */
     public function index(): array
     {
         return $this->client->userRequest('GET', 'playlists/'.$this->playlistId.'/items');
+    }
+
+    /**
+     * Accesses lightpath operations for playlist items
+     *
+     * @return PlaylistLightPathResource Resource for lightpath operations
+     */
+    public function lightpath(): PlaylistLightPathResource
+    {
+        return new PlaylistLightPathResource($this->client, $this->playlistId);
     }
 }
